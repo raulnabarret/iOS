a007e236925d9826d828e3600cfa6a675046a236
raulnabarret <raulnabarret@gmail.com>
2015-10-12 21:16:41 -0500

    Operadores unarios

--- operadoresBasicos.playground/Contents.swift
+++ operadoresBasicos.playground/Contents.swift
@@ -2,17 +2,33 @@
 
 import UIKit
 
+//Operadores binarios
+
 var ancho = 15
 var alto = 10
 
 let area = ancho * alto
-
 let areaMetros = Double (area) / 3.2808
 
 let sillas = 15
-
 print(sillas % 6)
 
+//Procedencia de Operadores
+
 var numero = 200+200-(((5*2)/3)%9)
+
+//Operadores Unarios
+
+var numeroDeVidas = 5
+numeroDeVidas++
+numeroDeVidas
+numeroDeVidas += 1
+++numeroDeVidas
+numeroDeVidas
+--numeroDeVidas
+-numeroDeVidas
+
+var flag : Bool = true
+!flag
 
 
