49c19ef9cecf564683391dde2561f2d386dba34b
raulnabarret <raulnabarret@gmail.com>
2015-10-12 21:05:38 -0500

    Operadores básicos

---
+++ operadoresBasicos.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var ancho = 15
+var alto = 10
+
+let area = ancho * alto
+
+let areaMetros = Double (area) / 3.2808
+
+let sillas = 15
+
+print(sillas % 6)
