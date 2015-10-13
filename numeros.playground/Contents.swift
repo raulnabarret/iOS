bc5c275ef0022820869958cc21643282322f6999
raulnabarret <raulnabarret@gmail.com>
2015-10-12 20:57:23 -0500

    Tipos de datos numéricos

---
+++ numeros.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var edad : Int = 21
+var PI : Double = 3.14159
+var g : Float = 5.678
+var flag : Bool = true
+print("Mi edad es \(edad) y la constante PI es \(PI)")
