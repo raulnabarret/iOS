9ea0df1c1ad7805ba86ca700d572a9dc1eaa40e6
raulnabarret <raulnabarret@gmail.com>
2015-10-18 18:29:57 -0500

    Clase de arreglos

---
+++ arreglos.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var idiomas = ["español", "ingles", "frances", "aleman"]
+
+idiomas.count
+
+idiomas += ["italiano", "italiano"]
+
+idiomas[0]
+
+idiomas[idiomas.count-1]
+
