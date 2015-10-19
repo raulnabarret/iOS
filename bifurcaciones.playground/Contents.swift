cac212178f29e471eac43b410495947403377c80
raulnabarret <raulnabarret@gmail.com>
2015-10-18 19:58:10 -0500

    If/else if/else

---
+++ bifurcaciones.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var meses = 1...12
+
+for mes in meses{
+
+    if mes == 1{
+        print("enero")
+    }else if mes == 2{
+        print("febrero")
+    }else{
+        print("Otros meses")
+    }
+}
