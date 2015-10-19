8c4a0a8f9b4bc0d636a04f7b843295dde55e805b
raulnabarret <raulnabarret@gmail.com>
2015-10-18 20:08:40 -0500

    Clase de switch

--- bifurcaciones.playground/Contents.swift
+++ bifurcaciones.playground/Contents.swift
@@ -6,11 +6,25 @@ var meses = 1...12
 
 for mes in meses{
 
-    if mes == 1{
+    
+    switch mes{
+    case 1...3:
+        print("Invierno" )
+    case 4...6:
+        print("Primavera")
+    case 7...9:
+        print("Verano")
+    case 10...12:
+        print("Invierno")
+    default:
+        print("Otro")
+    }
+    
+    /*if mes == 1{
         print("enero")
     }else if mes == 2{
         print("febrero")
     }else{
         print("Otros meses")
-    }
+    }*/
 }
