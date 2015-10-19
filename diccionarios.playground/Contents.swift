85802944068cab8d72611e51e8ac2eaa51043cb3
raulnabarret <raulnabarret@gmail.com>
2015-10-18 19:07:32 -0500

    Clase de Diccionarios

---
+++ diccionarios.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var idiomas = ["fr" : "frances", "it" : "italiano", "en" : "inglés", "sp" : "español"]
+
+idiomas ["en"]
+
+idiomas ["sp"] = "expañol latino"
+
+idiomas
+
+let italiano = idiomas.removeValueForKey("it")
+
+idiomas
+
+idiomas["pr"]
+
