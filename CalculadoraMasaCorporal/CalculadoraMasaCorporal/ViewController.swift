cae4e158a37d5692980317ff8ce60440dda19dfe
raulnabarret <raulnabarret@gmail.com>
2016-01-01 10:58:09 -0600

    Metodo para calcular IMC

--- CalculadoraMasaCorporal/CalculadoraMasaCorporal/ViewController.swift
+++ CalculadoraMasaCorporal/CalculadoraMasaCorporal/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var peso: UITextField!
+    
+    @IBOutlet weak var estatura: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        peso.delegate = self
+        estatura.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func textFieldDoneWriting(sender: UITextField) {
+    
+        sender.resignFirstResponder()
+        
+    }
 
+    @IBAction func calcularIMC(sender: AnyObject) {
+        var IMC : Double
+        
+        let pesoLocal : Double?
+        pesoLocal = Double(self.peso.text!)!
+        
+        let estaturaLocal : Double
+        estaturaLocal = Double(self.estatura.text!)!
+        
+        IMC = pesoLocal!/(estaturaLocal*estaturaLocal)
+        print("Tu IMC es \(IMC)")
+        
+    }
 
 }
 
